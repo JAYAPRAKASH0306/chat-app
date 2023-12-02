c7e788819d0f074c241b865bdd9f146c8cbf9a2d
JAYAPRAKASH0306 <jayaprakashk0306@gmail.com>
2023-12-02 23:31:20 +0530

    commited

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://jpchattify-app.netlify.app";
+export const host = "https://chattify-1.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
